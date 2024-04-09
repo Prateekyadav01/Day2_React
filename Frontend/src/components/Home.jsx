fc2dda58a1d2ffecd1114c1568929a53bd55a20d
PrateekTechmatys <prateeky562@gmail.com>
2024-04-09 22:54:43 +0530

    done

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import logo from '../assets/images/Netflix_Logo_PMS.png'
-
+import {useSelector} from 'react-redux'
 
 const Home = () => {
+  const selector = useSelector((state)=>{ return state.user});
+  console.log(selector);
   return (
     <div>
       <img src={logo} alt="logo" className='absolute left-0 w-3/12 z-10' />
