6c9ab9bdb21d6f603ba2c09ab3dc05641db82d05
PrateekTechmatys <prateeky562@gmail.com>
2024-04-09 09:56:02 +0530

    login

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './Home'
+import ImageBackground from "../assets/images/Background.jpg";
+
+
+
+const Login = () => {
+  return (
+    <div>
+      <Home/>
+      <img src={ImageBackground} alt="" />
+    </div>
+  )
+}
+
+export default Login
