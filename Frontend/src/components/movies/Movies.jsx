a2aa99c45c659e5ac44d18f8a0c9e024673b2bfa
PrateekTechmatys <prateeky562@gmail.com>
2024-04-18 16:23:24 +0530

    done

--- Frontend/src/components/movies/Movies.jsx
+++ Frontend/src/components/movies/Movies.jsx
@@ -7,6 +7,9 @@ const Movies = () => {
 
   return (
     <div className='flex flex-wrap justify-evenly gap-4 p-4 bg-black'>
+
+      
+
       {movieData &&
         movieData.map((movie) => (
           <Card key={movie.id} data={movie.poster_path} title={movie.title} />
