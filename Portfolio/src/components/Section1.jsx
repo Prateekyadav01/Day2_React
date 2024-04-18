7ba07d46678c6ed2b9d8a07b0cd4fd67cd621ecd
PrateekTechmatys <prateeky562@gmail.com>
2024-04-18 20:03:05 +0530

    starting of project porfolio

---
+++ Portfolio/src/components/Section1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Theme from './Section1/Theme';
+
+const Section1 = () => {
+  return (
+   <div className='w-[40%] items-center flex flex-col gap-2 border border-solid border-red-500'>
+      <Theme/>
+   </div>
+  );
+}
+
+export default Section1;
